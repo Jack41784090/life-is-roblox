c34dd55bd694ba302fd3ab3e0fc5796512d0b4c5
Jack41784090 <tszmingjack@gmail.com>
2025-09-24 21:54:36 -0700

    Remove unused shadow test client code

--- src/client/shadowtest.client.ts
+++
@@ -1,5 +0,0 @@
-// import { ShadowMachine } from "shared/class/ShadowMachine";
-
-// const testPart = game.Workspace.WaitForChild("Test2").WaitForChild("New Plain").WaitForChild("obj") as Part;
-// const shadowMachine = new ShadowMachine(testPart);
-// shadowMachine.updateShadow(2);
