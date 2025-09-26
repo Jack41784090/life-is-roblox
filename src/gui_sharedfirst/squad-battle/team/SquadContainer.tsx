76678930676b0bad25625e49c75ce9047b60912d
Jack41784090 <tszmingjack@gmail.com>
2025-09-25 18:29:45 -0700

    Improve SquadContainer and TeamContainer layouts

--- src/gui_sharedfirst/squad-battle/team/SquadContainer.tsx
+++ src/gui_sharedfirst/squad-battle/team/SquadContainer.tsx
@@ -5,36 +5,34 @@ import SquadDisplay from "../squad/SquadDisplay";
 interface SquadContainerProps {
     squads: Squad[];
     teamName: string;
+    Size?: UDim2;
 }
 
 function SquadContainer(props: SquadContainerProps) {
     return (
         <frame
-            Size={new UDim2(1, 0, 1, 0)}
-            Position={new UDim2(0, 0, .5, 0)}
-            AnchorPoint={new Vector2(0, .5)}
-            BackgroundTransparency={1}
+            Size={props.Size || new UDim2(1, 0, 1, 0)}
+            Position={UDim2.fromScale(0, 1)}
+            AnchorPoint={new Vector2(0, 1)}
+            BackgroundTransparency={.8}
         >
             <uilistlayout
                 FillDirection={Enum.FillDirection.Horizontal}
                 SortOrder={Enum.SortOrder.LayoutOrder}
                 HorizontalAlignment={Enum.HorizontalAlignment.Center}
+                VerticalAlignment={Enum.VerticalAlignment.Center}
+                // ItemLineAlignment={Enum.ItemLineAlignment.Center}
+                Padding={new UDim(0, 10)}
             />
 
             {props.squads.map((squad, squadIndex) => (
-                <frame
-                    key={`${props.teamName}-${squad.name}`}
-                    Size={new UDim2(1, 0, 1, 0)}
-                    BackgroundTransparency={1}
-                    LayoutOrder={squadIndex}
-                >
-                    <SquadDisplay
-                        name={squad.name}
-                        team={squad.team}
-                        entities={squad.entities}
-                        position={new UDim2(0, 0, 0, 0)}
-                    />
-                </frame>
+                <SquadDisplay
+                    name={squad.name}
+                    team={squad.team}
+                    entities={squad.entities}
+                    teamSize={props.squads.size()}
+                // position={new UDim2(0, 0, 0, 0)}
+                />
             ))}
         </frame>
     );
--- src/gui_sharedfirst/squad-battle/team/TeamContainer.tsx
+++ src/gui_sharedfirst/squad-battle/team/TeamContainer.tsx
@@ -1,7 +1,6 @@
 import React from "@rbxts/react";
 import { Squad } from "squad-battle/Squad";
 import SquadContainer from "./SquadContainer";
-import TeamHeader from "./TeamHeader";
 
 interface TeamContainerProps {
     teamName: string;
@@ -11,22 +10,26 @@ interface TeamContainerProps {
 }
 
 function TeamContainer(props: TeamContainerProps) {
+    const headYSize = .1;
+    const bodyYSize = 1 - headYSize;
     return (
         <frame
             key={props.teamName}
-            Size={new UDim2(.8, 0, .5, 0)}
+            Size={new UDim2(.9, 0, .5, 0)}
             BackgroundColor3={props.teamColor}
             BackgroundTransparency={0.8}
             BorderColor3={props.teamColor}
             BorderSizePixel={2}
             LayoutOrder={props.teamIndex}
         >
-            <TeamHeader
+            {/* <TeamHeader
+                Size={UDim2.fromScale(1, headYSize)}
                 teamName={props.teamName}
                 squadCount={props.squads.size()}
-            />
+            /> */}
 
             <SquadContainer
+                Size={UDim2.fromScale(1, 1)}
                 squads={props.squads}
                 teamName={props.teamName}
             />
