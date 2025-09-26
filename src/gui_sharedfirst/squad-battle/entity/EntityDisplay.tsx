44f6b8325f6082cfba5d1b944ebd10223ad2e821
Jack41784090 <tszmingjack@gmail.com>
2025-09-25 18:29:45 -0700

    Update EntityDisplay to use PlayerPortrait component

--- src/gui_sharedfirst/squad-battle/entity/EntityDisplay.tsx
+++ src/gui_sharedfirst/squad-battle/entity/EntityDisplay.tsx
@@ -1,8 +1,6 @@
 import React from "@rbxts/react";
 import { EntityBaseStats, EntityChangeableStats } from "squad-battle/type";
-import { getTeamColor } from "../shared/utils";
-import EntityInfo from "./EntityInfo";
-import EntityStats from "./EntityStats";
+import PlayerPortrait from "./PlayerPortrait";
 
 interface EntityDisplayProps {
     name: string;
@@ -10,28 +8,35 @@ interface EntityDisplayProps {
     team: string;
     baseStats: EntityBaseStats;
     changeableStats: EntityChangeableStats;
-    position?: UDim2;
+    // position?: UDim2;
 }
 
 function EntityDisplay(props: EntityDisplayProps) {
     return (
         <frame
-            Position={props.position || new UDim2(0, 0, 0, 0)}
-            Size={new UDim2(.25, 0, 1, 0)}
-            BackgroundColor3={getTeamColor(props.team)}
-            BackgroundTransparency={0.1}
-            BorderColor3={new Color3(0, 0, 0)}
-            BorderSizePixel={1}
+            Position={new UDim2(0, 0, 0, 0)}
+            Size={new UDim2(.25, 0, .8, 0)}
+            BackgroundColor3={new Color3(0.2, 0.2, 0.2)}
+            BackgroundTransparency={0}
+        // BorderColor3={new Color3(0, 0, 0)}
+        // BorderSizePixel={1}
         >
-            <uicorner CornerRadius={new UDim(1)} />
+            {/* <uiaspectratioconstraint AspectRatio={1} />
+            <uicorner CornerRadius={new UDim(1)} /> */}
+            <PlayerPortrait
+                // entityId={"props.playerID"}
+                entityId="entity_adalbrecht"
+                hp={props.changeableStats.HP}
+                maxHP={props.changeableStats.HP()}
+            />
 
-            <EntityInfo
+            {/* <EntityInfo
                 name={props.name}
                 playerID={props.playerID}
                 changeableStats={props.changeableStats}
-            />
+            /> */}
 
-            <EntityStats changeableStats={props.changeableStats} />
+            {/* <EntityStats changeableStats={props.changeableStats} /> */}
         </frame>
     );
 }
