33ea6c677ee885463c9e936f41a3d6791a3b3656
Jack41784090 <tszmingjack@gmail.com>
2025-09-25 18:29:45 -0700

    Update header components with flexible sizing support

--- src/gui_sharedfirst/squad-battle/BattleHeader.tsx
+++ src/gui_sharedfirst/squad-battle/BattleHeader.tsx
@@ -1,15 +1,16 @@
 import React from "@rbxts/react";
 
 interface BattleHeaderProps {
+    Size?: UDim2;
     currentRound?: number;
 }
 
 function BattleHeader(props: BattleHeaderProps) {
     return (
         <textlabel
-            Size={new UDim2(1, 0, 0, 40)}
+            Size={props.Size || new UDim2(1, 0, .2, 0)}
             Position={new UDim2(0, 0, 0, 0)}
-            BackgroundTransparency={1}
+            BackgroundTransparency={0}
             Text={`Squad Battle${props.currentRound ? ` - Round ${props.currentRound}` : ""}`}
             TextColor3={new Color3(1, 1, 1)}
             TextScaled={true}
--- src/gui_sharedfirst/squad-battle/team/TeamHeader.tsx
+++ src/gui_sharedfirst/squad-battle/team/TeamHeader.tsx
@@ -3,12 +3,13 @@ import React from "@rbxts/react";
 interface TeamHeaderProps {
     teamName: string;
     squadCount: number;
+    Size?: UDim2;
 }
 
 function TeamHeader(props: TeamHeaderProps) {
     return (
         <textlabel
-            Size={new UDim2(1, 0, 0, 30)}
+            Size={props.Size || new UDim2(1, 0, 0, 30)}
             BackgroundTransparency={1}
             Text={`Team: ${props.teamName} (${props.squadCount} squads)`}
             TextColor3={new Color3(1, 1, 1)}
