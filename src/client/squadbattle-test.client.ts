789245d419974e4c221266da26679ab3c64befba
Jack41784090 <tszmingjack@gmail.com>
2025-09-30 18:12:12 -0700

    test: refactor squad battle test with improved entity setup

--- src/client/squadbattle-test.client.ts
+++ src/client/squadbattle-test.client.ts
@@ -1,80 +1,81 @@
 import { getDummyStats } from "shared/utils";
 import { SquadBattleInstance } from "squad-battle";
-import { SquadBattleConfig, SquadConfig } from "squad-battle/type";
+import { SquadBattle } from "squad-battle/Battle";
+import { SquadBattleGraphics } from "squad-battle/Graphics";
+import { EntityUpdate, SquadBattleConfig, SquadConfig } from "squad-battle/type";
 
-// const sb = new SquadBattleInstance({
-//     squads: {
-//         'team1': [{
-//             entities: [
-//                 {
-//                     playerID: 1,
-//                     stats: getDummyStats(),
-//                     team: 'team1',
-//                     name: 'A1'
-//                 },
-//                 {
-//                     playerID: 3,
-//                     stats: getDummyStats(),
-//                     team: 'team1',
-//                     name: 'A2'
-//                 },
-//                 {
-//                     playerID: 5,
-//                     stats: getDummyStats(),
-//                     team: 'team1',
-//                     name: 'A3'
-//                 },
-//                 {
-//                     playerID: 7,
-//                     stats: getDummyStats(),
-//                     team: 'team1',
-//                     name: 'A4'
-//                 }
-//             ],
-//             name: "Squad1"
-//         }],
-//         'team2': [{
-//             entities: [
-//                 {
-//                     playerID: 2,
-//                     stats: getDummyStats(),
-//                     team: 'team2'
-//                 }
-//             ],
-
-//             name: "Squad2"
-//         }]
-//     }
-// })
-
-const team_count = 2;
-const squad_count = 1
-const squad_numbers = 5;
-const config: SquadBattleConfig = {
-    teams: {}
-}
-
-for (let tc = 0; tc < team_count; tc++) {
-    config.teams[`team${tc + 1}`] = []
-    const squads = config.teams[`team${tc + 1}`];
-    for (let sc = 0; sc < squad_count; sc++) {
-        const squad_config: SquadConfig = {
-            name: `squad${tc + 1}-${string.char(65 + sc)}`,
-            entities: []
-        };
-        for (let sn = 0; sn < squad_numbers; sn++) {
-            squad_config.entities.push({
-                playerID: sn + math.random(),
-                stats: getDummyStats(),
-                team: `team${tc + 1}`,
-                name: `tc${tc + 1}_${sc + 1}_${sn + 1}`
-            })
-        }
-
-        squads.push(squad_config);
+function test_simpleautobattle() {
+    const team_count = 2;
+    const squad_count = 1
+    const squad_numbers = 5;
+    const config: SquadBattleConfig = {
+        teams: {}
     }
+
+    for (let tc = 0; tc < team_count; tc++) {
+        config.teams[`team${tc + 1}`] = []
+        const squads = config.teams[`team${tc + 1}`];
+        for (let sc = 0; sc < squad_count; sc++) {
+            const squad_config: SquadConfig = {
+                name: `squad${tc + 1}-${string.char(65 + sc)}`,
+                entities: []
+            };
+            for (let sn = 0; sn < squad_numbers; sn++) {
+                squad_config.entities.push({
+                    playerID: tc + 1 + math.random(),
+                    stats: getDummyStats(),
+                    team: `team${tc + 1}`,
+                    name: `tc${tc + 1}_${sc + 1}_${sn + 1}`,
+                    logicType: 'Frontline'
+                })
+            }
+
+            squads.push(squad_config);
+        }
+    }
+
+    const sb = new SquadBattleInstance(config);
+    sb.autoBattle();
 }
 
-const sb = new SquadBattleInstance(config);
-sb.autoBattle();
+function test_fixedsequence() {
+    const sb = new SquadBattle({
+        teams: {
+            'team1': [{
+                entities: [{ playerID: 1, stats: getDummyStats(), team: 'team1', name: 'tc1_1_1', logicType: 'Frontline' }],
+                name: 'squad1-A'
+            }],
+            'team2': [{
+                entities: [{ playerID: 2, stats: getDummyStats(), team: 'team2', name: 'tc2_1_1', logicType: 'Frontline' }],
+                name: 'squad2-A'
+            }]
+        }
+    })
+    const sbg = new SquadBattleGraphics(sb)
 
+    const randomUpdates: EntityUpdate[] = [];
+    for (let i = 0; i < 10; i++) {
+        const u: EntityUpdate = math.random() < 0.5 ? {
+            source: 1,
+            affected: 2,
+            change: {
+                property: 'HP',
+                from: sb.getEntityByID(2)!.get_changeableStat_num('HP'),
+                to: sb.getEntityByID(2)!.get_changeableStat_num('HP') - 30,
+            }
+        } : {
+            source: 2,
+            affected: 2,
+            change: {
+                property: 'LOC',
+                from: sb.getEntityByID(2)!.get_changeableStat_num('LOC'),
+                to: math.max(1, math.min(3, sb.getEntityByID(2)!.get_changeableStat_num('LOC') + (math.random() < 0.5 ? 1 : -1))),
+            }
+        }
+        randomUpdates.push(u)
+    }
+    sbg.render(randomUpdates)
+}
+
+// test_fixedsequence()
+test_simpleautobattle();
