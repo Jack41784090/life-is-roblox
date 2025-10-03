f68092c25ec0eb0807a167079978f8b2d7f9c7cf
Jack41784090 <tszmingjack@gmail.com>
2025-10-03 00:35:05 -0700

    Refactor EntityDisplay to use EntityPortraitWithIndicators

--- src/gui_sharedfirst/squad-battle/entity/EntityDisplay.tsx
+++ src/gui_sharedfirst/squad-battle/entity/EntityDisplay.tsx
@@ -1,28 +1,22 @@
 // filepath: c:\Users\tszmi\Documents\Code\roblox-game\src\gui_sharedfirst\new_components\battle\statusBar\playerPortrait\index.tsx
-import { useMotion, useViewport } from "@rbxts/pretty-react-hooks";
-import React, { useEffect, useState } from "@rbxts/react";
+import { useMotion } from "@rbxts/pretty-react-hooks";
+import React, { useEffect } from "@rbxts/react";
 import Bar from "gui_sharedfirst/new_components/loading/components/bar";
 import { Reality } from "shared/class/battle/Systems/CombatSystem/types";
 import { findEntityPortrait, springs } from "shared/utils";
 import { EntityDisplayProps } from "../type";
 import EntityCircleBar from "./EntityCircleBar";
-import EntityIndicators from "./EntityIndicators";
-import EntityPortrait from "./EntityPortrait";
+import EntityPortraitWithIndicators from "./EntityPortraitWithIndicators";
 
 /**
  * Circular player portrait with HP bar surrounding it.
  * The HP bar is a circular arc that surrounds 25% of the portrait in the top-right quadrant when full.
  */
-function PlayerPortrait(props: EntityDisplayProps) {
-    warn(` | | | | EntityDisplay ${math.random() * 100 / 100}`);
-
+function EntityDisplay(props: EntityDisplayProps) {
     const { entity } = props;
-    const viewport = useViewport();
     const [hpRatio, hpMotion] = useMotion(1);
     const [orgRatio, orgMotion] = useMotion(1);
-    const [isAttacking, setIsAttacking] = useState(false);
-    const [isRetreating, setIsRetreating] = useState(false);
-    const [isDying, setIsDying] = useState(false);
+
     const hp = props.entity.changeableStats.HP();
     const org = entity.changeableStats.ORG();
     const maxHP = props.entity.calculateRealityValue(Reality.HP);
@@ -36,53 +30,7 @@ function PlayerPortrait(props: EntityDisplayProps) {
         orgMotion.spring(org / maxORG, springs.slow);
     }, [org]);
 
-    // Process entity updates to create damage indicators
-    useEffect(() => {
-        if (!props.entityUpdates || props.entityUpdates.size() === 0) return;
-        props.entityUpdates.forEach((update, index) => {
-            switch (update.change.property) {
-                case 'DIE':
-                case 'RETREAT':
-                case 'LEAVE': {
-                    if (update.change.property === 'DIE') {
-                        setIsDying(true);
-                    }
-                    else if (update.change.property === 'LEAVE') {
-                        setIsRetreating(true);
-                    }
-                    break;
-                }
 
-                case 'HP': {
-                    const dhp = update.change.to - update.change.from;
-                    if (update.source === props.entity.playerID) {
-                        if (dhp < 0) {
-                            update.onComplete = () => {
-                                warn("Oncomplete called")
-                                setIsAttacking(true);
-                                update.done = true;
-                            }
-                        }
-                        else if (dhp > 0) {
-                            // update.onComplete = () => {
-
-                        }
-                    }
-                }
-
-                case 'LOC': {
-                    const dloc = update.change.to - update.change.from;
-                    // props.transferFunction(props.entity, update.change.to as SquadEntityInSquadLocation);
-                    if (dloc > 0) {
-                        setIsRetreating(true);
-                    }
-                    else {
-                        setIsRetreating(false);
-                    }
-                }
-            }
-        });
-    }, [props.entityUpdates]);
 
     // Find portrait using utility function
     const portraitImage = findEntityPortrait(props.entity.stats.id, 'neutral');
@@ -97,29 +45,19 @@ function PlayerPortrait(props: EntityDisplayProps) {
                 Size={UDim2.fromScale(1, 0.2)}
                 Text={props.entity.name}
                 BackgroundTransparency={1}
-                TextColor3={isDying ? new Color3(1, 0, 0) : new Color3(1, 1, 1)}
+                TextColor3={new Color3(1, 1, 1)}
                 TextScaled={true}
             />
-            {
-                isDying || isRetreating ?
-                    <></> : (
-                        <>
-                            <EntityCircleBar hpRatio={orgRatio} />
-                            <Bar progress={hpRatio} />
-                        </>
-                    )
-            }
-            <EntityPortrait
+            <EntityCircleBar hpRatio={orgRatio} />
+            <Bar progress={hpRatio} />
+            <EntityPortraitWithIndicators
                 portraitImage={portraitImage}
-                isDying={isDying}
-                isRetreating={isRetreating}
-                isAttacking={isAttacking}
                 upsideDown={props.upsideDown}
-                setAttacking={setIsAttacking}
+                updates={props.entityUpdates ?? []}
+                myID={props.entity.playerID}
             />
-            <EntityIndicators updates={props.entityUpdates ?? []} myID={props.entity.playerID} />
         </frame>
     );
 }
 
-export = PlayerPortrait;
+export = EntityDisplay;
