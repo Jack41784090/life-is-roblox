9639887b6402ce9f14b2a3fd8a91d7759f2096f0
Jack41784090 <tszmingjack@gmail.com>
2025-10-03 00:35:05 -0700

    Add animationTrigger to ProtoIndicator type

--- src/gui_sharedfirst/squad-battle/entity/types.ts
+++ src/gui_sharedfirst/squad-battle/entity/types.ts
@@ -15,6 +15,7 @@ export interface ProtoIndicator {
     position: UDim2;
     atSecond: number;
     onComplete?: () => void;
+    animationTrigger?: string;
 }
 
 export type EntityUpdateIndicator =
