89db15b3c19a7b0aacb0d2e67e4950136d788c08
Jack41784090 <tszmingjack@gmail.com>
2025-10-03 20:20:47 -0700

    Adjust indicator timing delay in SquadBattleInstance

--- src/squad-battle/index.ts
+++ src/squad-battle/index.ts
@@ -38,7 +38,7 @@ export class SquadBattleInstance {
                 }
             })
 
-            wait(delayBetweenIndicator * (updates.size()));
+            wait(1.6 * delayBetweenIndicator * (updates.size()));
             print('')
         }
     }
