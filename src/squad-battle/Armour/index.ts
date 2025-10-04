b87f34578bfea23bc931bd28bb0a2c0938d9f86c
Jack41784090 <tszmingjack@gmail.com>
2025-10-04 14:41:14 -0700

    Implement Armour class in combat system

--- src/squad-battle/Armour/index.ts
+++ src/squad-battle/Armour/index.ts
@@ -1,50 +1,51 @@
-// import Logger from "shared/utils/Logger";
-// import { DamageType } from "../Ability/types";
-// import { ArmourConfig, ArmourState } from "./types";
+import { DamageType } from "shared/class/battle/Systems/CombatSystem/Ability/types";
+import { ArmourConfig, ArmourState } from "shared/class/battle/Systems/CombatSystem/Armour/types";
+import Logger from "shared/utils/Logger";
+import { iArmour } from "./type";
 
-// export default class Armour {
-//     private logger = Logger.createContextLogger("Armour");
-//     private DV: number;
-//     private PV: number;
-//     private resistance: Map<DamageType, number> = new Map();
+export default class Armour implements iArmour {
+    private logger = Logger.createContextLogger("Armour");
+    private DV: number;
+    private PV: number;
+    private resistance: Map<DamageType, number> = new Map();
 
-//     public static Unprotected(): Armour {
-//         return new Armour({
-//             DV: 12, PV: 5, resistance: new Map([
-//                 [DamageType.Cut, -0.2],
-//                 [DamageType.Impale, -0.2],
-//             ])
-//         });
-//     }
+    public static Unprotected(): Armour {
+        return new Armour({
+            DV: 12, PV: 5, resistance: new Map([
+                [DamageType.Cut, -0.2],
+                [DamageType.Impale, -0.2],
+            ])
+        });
+    }
 
-//     constructor({ DV, PV, resistance }: ArmourConfig) {
-//         this.DV = DV;
-//         this.PV = PV;
-//         this.resistance = resistance;
-//     }
+    constructor({ DV, PV, resistance }: ArmourConfig) {
+        this.DV = DV;
+        this.PV = PV;
+        this.resistance = resistance;
+    }
 
-//     public getDV(): number {
-//         return this.DV;
-//     }
+    public getDV(): number {
+        return this.DV;
+    }
 
-//     public getPV(): number {
-//         return this.PV;
-//     }
+    public getPV(): number {
+        return this.PV;
+    }
 
-//     public getRawDamageTaken(damageTypesArray: Record<DamageType, number>): number {
-//         let damage = 0;
-//         for (const [damageType, value] of pairs(damageTypesArray)) {
-//             damage += this.resistance.get(damageType) ? value * (1 - this.resistance.get(damageType)!) : value;
-//         }
+    public getRawDamageTaken(damageTypesArray: Record<DamageType, number>): number {
+        let damage = 0;
+        for (const [damageType, value] of pairs(damageTypesArray)) {
+            damage += this.resistance.get(damageType) ? value * (1 - this.resistance.get(damageType)!) : value;
+        }
 
-//         return damage;
-//     }
+        return damage;
+    }
 
-//     public getState(): ArmourState {
-//         return {
-//             DV: this.DV,
-//             PV: this.PV,
-//             resistance: this.resistance,
-//         };
-//     }
-// }
+    public getState(): ArmourState {
+        return {
+            DV: this.DV,
+            PV: this.PV,
+            resistance: this.resistance,
+        };
+    }
+}
