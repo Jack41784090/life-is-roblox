59e9f3b8f76692fa9ef24c8e3ba6431e6593f325
Jack41784090 <tszmingjack@gmail.com>
2025-10-12 16:23:03 -0700

    * Enhance OneClash with detailed logging for status effects and cleanup process * fundamentals of a working skill system

--- src/squad-battle/Battle/System/index.ts
+++ src/squad-battle/Battle/System/index.ts
@@ -26,8 +26,10 @@ export class OneClash implements iOneClash {
     }
 
     private applyEffect(statusEffectdata: iSkillEffect): EntityUpdate[] | undefined {
+        this.logger.debug(`Applying effect`, statusEffectdata);
         const target = this.targetManifestation();
         if (statusEffectdata.destroyOnTrigger) {
+            this.logger.debug(`Removing one-time effect`, statusEffectdata);
             const index = target.statusEffects.indexOf(statusEffectdata);
             if (index > -1) {
                 target.statusEffects.remove(index);
@@ -46,14 +48,13 @@ export class OneClash implements iOneClash {
                 break;
             }
             case 'Damage': {
+                this.logger.debug(`Applying damage effect`, statusEffectdata.effect);
                 const de = statusEffectdata.effect as DamageEffectData;
-                if (statusEffectdata.destroyOnTrigger) {
-                    // this.logger.debug(`Removing one-time effect`, statusEffectdata);
-                }
                 return target.damage(de.amount ?? 0, this.attacker.playerID);
             }
 
             case 'Heal': {
+                this.logger.debug(`Applying heal effect`, statusEffectdata.effect);
                 const h = target.heal(statusEffectdata.effect.amount ?? 0)
                 if (!h) return;
                 return [{
@@ -70,26 +71,41 @@ export class OneClash implements iOneClash {
         }
     }
 
+    private _seEffectDisconnects: (() => void)[] = [];
     private registerExistingSE() {
         this.attacker.statusEffects.forEach(se => {
-            this.eventBus.subscribe(se.trigger, () => {
+            this.logger.debug(`Registering existing status effect`, se);
+            const c = this.eventBus.subscribe(se.trigger, () => {
                 this.applyEffect(se)?.forEach(u => this.updates.push(u));
             });
+            this._seEffectDisconnects.push(c);
         })
         this.defender?.statusEffects.forEach(se => {
-            this.eventBus.subscribe(se.trigger, () => {
+            this.logger.debug(`Registering existing status effect`, se);
+            const c = this.eventBus.subscribe(se.trigger, () => {
                 this.applyEffect(se)?.forEach(u => this.updates.push(u));
             });
+            this._seEffectDisconnects.push(c);
         })
     }
 
     private registerSkillSE() {
         this.skill.effects.forEach(e => {
             if (e.affected === 'self') {
-                this.attacker.statusEffects.push(e);
+                this.logger.debug(`Registering skill effect`, e);
+                const c = this.eventBus.subscribe(e.trigger, () => {
+                    this.applyEffect(e)?.forEach(u => this.updates.push(u));
+                });
+                this._seEffectDisconnects.push(c);
             }
             else if (e.affected === 'target') {
-                this.targets.forEach(t => t.statusEffects.push(e));
+                this.targets.forEach(t => {
+                    this.logger.debug(`Registering skill effect`, e);
+                    const c = this.eventBus.subscribe(e.trigger, () => {
+                        this.applyEffect(e)?.forEach(u => this.updates.push(u));
+                    });
+                    this._seEffectDisconnects.push(c);
+                });
             }
         })
     }
@@ -100,7 +116,7 @@ export class OneClash implements iOneClash {
         return this.defender;
     }
 
-    private rollForHit(): EntityUpdate[] | undefined {
+    private rollForHit(): boolean {
         // this.eventBus.emit()
 
         const chosenWeapon = this.attacker.weapon; //temp
@@ -108,11 +124,12 @@ export class OneClash implements iOneClash {
         const armour = target.get_armour();
 
         const tryHit = chosenWeapon.getTotalHitValue(this.attacker);
-        const hitDef = armour.getDV();
+        // const hitDef = armour.getDV();
+        const hitDef = 0; //temp
         const rollOffence_hit = uniformRandom(0, tryHit);
         const rollDefence_hit = uniformRandom(0, hitDef);
         if (rollDefence_hit >= rollOffence_hit) {
-            return [{
+            this.updates.push({
                 source: this.attacker.playerID,
                 affected: target.playerID,
                 change: {
@@ -120,13 +137,13 @@ export class OneClash implements iOneClash {
                     from: -1,
                     to: -1,
                 }
-            }];
+            });
         }
-
+        return rollOffence_hit > rollDefence_hit;
         // return [];
     }
 
-    private rollForPierce(): EntityUpdate[] | undefined {
+    private rollForPierce(): boolean {
         const chosenWeapon = this.attacker.weapon; //temp
         const target = this.targetManifestation();
         const armour = target.get_armour();
@@ -136,7 +153,7 @@ export class OneClash implements iOneClash {
         const rollOffence_hit = uniformRandom(0, tryHit);
         const rollDefence_hit = uniformRandom(0, hitDef);
         if (rollDefence_hit >= rollOffence_hit) {
-            return [{
+            this.updates.push({
                 source: this.attacker.playerID,
                 affected: target.playerID,
                 change: {
@@ -144,49 +161,64 @@ export class OneClash implements iOneClash {
                     from: -1,
                     to: -1,
                 }
-            }];
+            });
         }
-
-        // return [];
+        return rollOffence_hit > rollDefence_hit;
     }
 
-    private damageCalculation() {
+    private damageCalculation(): void {
         const chosenWeapon = this.attacker.weapon; //temp
         const target = this.targetManifestation();
         const armour = target.get_armour();
         const dm = armour.getRawDamageTaken(chosenWeapon.getPotencyArrayDamage(target))
 
+        const d = target.damage(dm, this.attacker.playerID);
+        d.forEach(u => this.updates.push(u));
         this.eventBus.emit('OnBasicAttackHit', {
             attacker: this.attacker,
             defender: target,
             damage: dm,
             weapon: chosenWeapon,
         });
-        return target.damage(dm, target.playerID);
     }
 
     private removeDestroyedSE() {
-        this.attacker.statusEffects = this.attacker.statusEffects.filter(se => se.duration !== 0);
-        if (this.defender) this.defender.statusEffects = this.defender.statusEffects.filter(se => se.duration !== 0);
+        this.attacker.statusEffects = this.attacker.statusEffects.filter(se => {
+            this.logger.debug(`Removing status effect`, se);
+            return se.duration !== 0
+        });
+        if (this.defender) this.defender.statusEffects = this.defender.statusEffects.filter(se => {
+            this.logger.debug(`Removing status effect`, se);
+            return se.duration !== 0
+        });
+    }
+
+    private disconnectAll() {
+        this.logger.debug(`Disconnecting all status effect listeners`);
+        this._seEffectDisconnects.forEach(disconnect => disconnect());
     }
 
     commit(): EntityUpdate[] {
-        this.registerSkillSE();
         this.registerExistingSE();
-        const hit = this.rollForHit()?.forEach(u => this.updates.push(u));
+        this.registerSkillSE();
+
+        const hit = this.rollForHit();
         if (!hit) {
-            this.removeDestroyedSE();
-            return this.updates;
+            return this.cleanup();
         }
 
-        const pierce = this.rollForPierce()?.forEach(u => this.updates.push(u));
+        const pierce = this.rollForPierce();
         if (!pierce) {
-            this.removeDestroyedSE();
-            return this.updates;
+            return this.cleanup();
         }
 
-        const damage = this.damageCalculation()?.forEach(u => this.updates.push(u));
+        this.damageCalculation();
+        return this.cleanup();
+    }
+
+    private cleanup() {
         this.removeDestroyedSE();
+        this.disconnectAll();
         return this.updates;
     }
 }
